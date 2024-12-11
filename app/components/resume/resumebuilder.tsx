6d82d4eadbfd9617d158b8ba199fc8114513950b
dominikperlak <dominikperlak2@gmail.com>
2024-12-11 16:48:52 +0100

    feat:landing-page

--- app/components/resume/resumebuilder.tsx
+++ app/components/resume/resumebuilder.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import { ResumeForm } from './resumeform';
 import { ResumePreview } from './resumepreview';
-import { Button } from './ui/button';
+import { Button } from '../ui/button';
 import { useRouter } from 'next/navigation';
 
 const Builder = () => {
