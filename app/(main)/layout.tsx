223419ac8d16a54c51df1746426170312d16b87b
dominikperlak <dominikperlak2@gmail.com>
2024-12-11 12:14:56 +0100

    feat(layout) resume cv

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,8 +1,3 @@
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-import Sections from '../components/sections/Sections';
-import Settings from '../components/settings/Settings';
-
 export default function MainLayout({
   children,
 }: {
@@ -10,17 +5,10 @@ export default function MainLayout({
 }) {
   return (
     <div className="flex flex-col min-h-screen">
-      <Navbar />
       <main className="flex-grow flex">
-        <div className="flex-1">
-          <Sections />
-          {children}
-        </div>
-        <div className="flex-none">
-          <Settings />
-        </div>
+        <div className="flex-1">{children}</div>
+        <div className="flex-none"></div>
       </main>
-      <Footer />
     </div>
   );
 }
---
+++ app/builder/page.tsx
@@ -0,0 +1,5 @@
+const builder = () => {
+  return <h1></h1>;
+};
+
+export default builder;
