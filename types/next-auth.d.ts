c4adf17d4b033a4706eb4e07ccdad532b2d84261
dominikperlak <dominikperlak2@gmail.com>
2024-12-16 09:16:48 +0100

    feat:(github-integration)

---
+++ types/next-auth.d.ts
@@ -0,0 +1,12 @@
+import NextAuth from "next-auth"
+
+declare module "next-auth" {
+  interface Session {
+    accessToken?: string;
+    user?: {
+      email?: string | null;
+      name?: string | null;
+      image?: string | null;
+    };
+  }
+}
