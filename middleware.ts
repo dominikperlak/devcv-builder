3bb7c39921968353e17b8843b240cd76922d0950
dominik perlak <dominikperlak2@gmail.com>
2025-01-15 11:23:55 +0100

    fix(middleware)

--- middleware.ts
+++ middleware.ts
@@ -5,11 +5,9 @@ export async function middleware(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
   const path = req.nextUrl.pathname;
 
-  if (path.startsWith('/api/auth/callback')) {
-    return NextResponse.next();
-  }
 
-  if ((path === '/sign-in' || path === '/sign-up') && token) {
+
+  if (path === '/sign-in' && token) {
     return NextResponse.redirect(new URL('/my-resume', req.url));
   }
 
